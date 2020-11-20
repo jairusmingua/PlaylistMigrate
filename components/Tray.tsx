af39544beaf48dc2d178455ba7bd0d9e6838151a
Jairus <minguajairus@yahoo.com.ph>
2020-11-20 18:26:32 +0800

    🎨 [fixed] login button ui

--- components/Tray.tsx
+++ components/Tray.tsx
@@ -1,4 +1,4 @@
-import { Row, Col, Container, Media } from 'react-bootstrap';
+import { Row, Col, Container, Media,Button } from 'react-bootstrap';
 import { AuthType, OAuthType, Profile, Playlist, SpotifyProfile } from '../services/types';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
@@ -25,9 +25,9 @@ export default function Tray({ trayType }: AppProps) {
                     "Authorization": `Bearer ${window.localStorage.getItem(`${trayType}_access_token`)}`
                 }
             }).then(async (response) => {
-                if(response.status==500){
+                if (response.status == 500) {
                     clearStorage();
-                }else{
+                } else {
                     const _profile = await response.json();
                     setProfile(
                         _profile
@@ -38,9 +38,9 @@ export default function Tray({ trayType }: AppProps) {
                             "Authorization": `Bearer ${window.localStorage.getItem(`${trayType}_access_token`)}`
                         }
                     }).then(async (response) => {
-                        if(response.status==500){
+                        if (response.status == 500) {
                             clearStorage();
-                        }else{
+                        } else {
                             const _playlist: Array<Playlist> = await response.json();
                             setPlaylistItems(
                                 _playlist
@@ -71,26 +71,26 @@ export default function Tray({ trayType }: AppProps) {
                     </Col>
                 </Row> */}
                 <Row className="h-50 tray w-100 mt-1 m-0 p-0">
-                    <Row xs={12} className="h-auto m-0" style={{width:"100%"}}>
+                    <Row xs={12} className="h-auto m-0" style={{ width: "100%" }}>
                         <div className="p-3 w-100">
 
-                        <Col className="p-0 m-0">
-                            <img src={`${trayType}.png`} className="ml-0 float-left rounded-circle profile-pic"></img>
-                        </Col>
-                        <Col className="pr-2">
-                            <Row className="float-right">
+                            <Col className="p-0 m-0">
+                                <img src={`${trayType}.png`} className="ml-0 float-left rounded-circle profile-pic"></img>
+                            </Col>
+                            <Col className="pr-2">
+                                <Row className="float-right">
 
-                                <h6>{profile.name}</h6>
-                                <img className="rounded-circle profile-pic" src={profile.profilePic_url.toString()} />
+                                    <h6>{profile.name}</h6>
+                                    <img className="rounded-circle profile-pic" src={profile.profilePic_url.toString()} />
 
-                            </Row>
-                        </Col>
+                                </Row>
+                            </Col>
 
                         </div>
                     </Row>
                     <Row xs={12} className="w-100 m-0 pb-3" style={{ height: "75%" }}>
                         {playlistItems != [] &&
-                        
+
                             <ul className="mx-2 py-0 px-2 list-unstyled w-100 h-100 playlist-list">
                                 {playlistItems.map((items) => {
                                     return <PlaylistItem key={items.id} playlist={items}></PlaylistItem>
@@ -103,9 +103,12 @@ export default function Tray({ trayType }: AppProps) {
                 </Row>
             </>
             :
-            <Row>
-                {`Login to ${trayType == "spotify" ? 'Spotify' : 'Youtube'}`}
-                <Link href={`/api/login?${AuthType.queryString}=${trayType}`}>Login</Link>
+            <Row className="h-50 tray w-100 mt-1 m-0 p-0">
+                <div className="align-self-center position-fixed" style={{left:"50%",transform:"translate(-50%,0%)"}}>
+
+                    <Link href={`/api/login?${AuthType.queryString}=${trayType}`}><Button>{`Login to ${trayType == "spotify" ? 'Spotify' : 'Youtube'}`}</Button></Link>
+
+                </div>
             </Row>
         }
     </>
