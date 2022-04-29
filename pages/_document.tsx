d41e3bc570ffd9ffc428400aa636fa1a8cfc4ec6
jairusmingua <minguajairus@yahoo.com.ph>
2022-04-30 03:40:05 +0800

    fix manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png"></link>
+                    <meta name="theme-color" content="#131A13" />
+
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" integrity="sha384-ejwKkLla8gPP8t2u0eQyL0Q/4ItcnyveF505U0NIobD/SMsNyXrLti6CWaD0L52l" crossOrigin={"true"}></link>
